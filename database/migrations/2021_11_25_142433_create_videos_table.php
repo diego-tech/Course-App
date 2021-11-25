9bf5257143e787ace8b875ecb931f6a5387a9b48
Diego Muñoz Herranz <diego171200@gmail.com>
2021-11-25 15:47:06 +0100

    Initial Commit

---
+++ database/migrations/2021_11_25_142433_create_videos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 60)->require();
+            $table->string('video_thumbnail', 60);
+            $table->string('videolink', 150)->require();
+            $table->foreignId('course_id')->constrained()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
